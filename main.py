467a33eb24b7f5154a9b7ef25f8589d97b2cebd9
Floppymnt <96642794+Floppymnt@users.noreply.github.com>
2025-07-12 05:25:51 -0500

    fixed init() func

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-import sys
+import sys , os
+
 from stats import report
 
 def arg_check(input):# checks if arguments are passed in
     if len(input) > 1:
         return input
 
+def init():
+    # This function initializes the codebase
+    #check if books directory exists, if not create it
+    try:
+        with open("books", "r") as f:
+            pass
+    except FileNotFoundError:
+        with open("books", "w") as f:
+            print(f"Books directory created. please add your book txt files to {os.path.abspath(f.name)}")
+            pass
 
 
 def get_book_text(path2file):
@@ -17,6 +28,7 @@ def get_book_text(path2file):
 
 
 def main():
+
 
     try:
         input = arg_check(sys.argv[1])
